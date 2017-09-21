2edfe29df41befa80d942de7d25eaf5b60d52bdf
Vladimir <vladimir@pravdabox.net>
2017-09-21 15:17:56 +0200

    adjust dns lookup for ip

--- engine/files/dns_lookup.sh
+++ engine/files/dns_lookup.sh
@@ -1,4 +1,5 @@
 #!/bin/ash
 
-[[ -n "$1" ]] || exit 0
-nslookup "$1" 127.0.0.1 | tail -n 1 | awk '{ print $4 }'
+[[ -n "$2" ]] || exit 0
+name=$(nslookup "$2" 127.0.0.1 | tail -n 1 | awk '{ print $4 }')
+echo "$1	$name"
